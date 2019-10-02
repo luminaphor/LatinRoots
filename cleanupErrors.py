46f18254cce6cf1258ab1896f8705c5413b8f5d6
Jocelyn <43040516+pseudoEternity@users.noreply.github.com>
2019-10-02 19:41:33 -0400

    Update cleanupErrors.py

--- cleanupErrors.py
+++ cleanupErrors.py
@@ -1,12 +1,13 @@
 newFile=open("definitions.txt","r")
-newFile2=open("corrected.txt","w")
+newFile2=open("corrected.txt","w") #creates new file so we can clean up the original definitions file.
 
-getLines=newFile.readlines()
+getLines=newFile.readlines() #reads in all definitions.
 
-knownDupes=[12,218,558,602,653,685,825,1016,1068,1121,1125]
-replacements={}
+knownDupes=[12,218,558,602,653,685,825,1016,1068,1121,1125] #this was found manually, but it is a list of all words that appear twice that
+#are not meant to appear twice
+replacements={} #empty list to fill in replacements
 
-wordstoreplace= [
+wordstoreplace= [ #these are the replacements that are each filled in for the found duplicates
     "6 bitter, pungent, sharp, sour",
     "33 be dry",
     "74 stench",
@@ -40,11 +41,11 @@ wordstoreplace= [
     "1200 green",
     "1210 wish"]
 
-for word in wordstoreplace:
-    word=word.split(" ")
-    k=int(word[0])-1
-    body=word[1:]
-    body=" ".join(body)
+for word in wordstoreplace: #for each word that appears in the replacement list
+    word=word.split(" ") #we split it to get rid of the number on the front
+    k=int(word[0])-1 #grabs the indeces for each replacement word
+    body=word[1:] #grabs the definition associated with the index
+    body=" ".join(body) #joins any words that are currently separate in the definition
     
     replacements[k]=body
     
